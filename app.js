c12201545fa4d36fefda1561a7a38085fa92e5ba
Ming <ximing.dai@linecorp.com>
2018-06-21 14:29:57 +0900

    validate command & token

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const bodyParser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
+const config = require('./config')
 
 const app = new Koa()
 const router = new Router()
 
-// { "command": "" }
+// { "command": "", "token": "" }
 router.post('/call', async ctx => {
   const body = ctx.request.body
-  // TODO: white list of command
-  const { command } = body
+  const { command, token } = body
+  ctx.assert(token === config.token, 401, 'Unauthorized')
+  ctx.assert(config.commands.includes(command), 404, 'Command not found')
   const { stdout, stderr } = await exec(`python BlackBeanControl/BlackBeanControl.py -c '${command}'`)
   ctx.body = 'ok'
 })
---
+++ config.sample.js
@@ -0,0 +1,13 @@
+module.exports = {
+  token: "",
+  commands: [
+    "turn on the light",
+    "light on",
+    "turn off the light",
+    "light off",
+    "brighten the light",
+    "dim the light",
+    "light color warmer",
+    "light color colder"
+  ]
+}
